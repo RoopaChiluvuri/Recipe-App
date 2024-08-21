dd9b86b658e89c4e3ff4d7313c8e51aa2f127a1f
RoopaChiluvuri <roopasree2509@gmail.com>
2024-08-21 10:52:06 -0700

    added about section

--- recipes/views.py
+++ recipes/views.py
@@ -5,3 +5,7 @@ from django.shortcuts import render, HttpResponse
 
 def home(request):
     return HttpResponse("<h1>Welcome to my Recipes App!<h1> ")
+
+
+def about(request):
+    return HttpResponse("<h1>This is a recipes app to keep track of your recipes.</h1>")
