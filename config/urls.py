5c79687bf111197512f9a4dd2b0ee5551800fa38
RoopaChiluvuri <roopasree2509@gmail.com>
2024-08-21 21:46:49 -0700

    creating register.html

--- config/urls.py
+++ config/urls.py
@@ -15,12 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from users import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('recipes.urls')),
-
-    # This line makes the url like 8000/recipes to see the home page
-    # path('recipes/', include('recipes.urls')),
+    path('register/', user_views.register, name="user-register"),
 
 ]
