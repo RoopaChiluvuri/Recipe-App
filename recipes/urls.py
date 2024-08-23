fdee83620819f552c46f56266f91b941d6c7c04a
RoopaChiluvuri <roopasree2509@gmail.com>
2024-08-22 17:09:33 -0700

    adding create recipe functionality

--- recipes/urls.py
+++ recipes/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.RecipeListView.as_view(), name="recipes-home"),
     path('recipe/<int:pk>/', views.RecipeDetailView.as_view(), name="recipes-detail"),
+    path('recipe/create/', views.RecipeCreateView.as_view(), name="recipes-create"),
     path('about/', views.about, name="recipes-about"),
 ]
