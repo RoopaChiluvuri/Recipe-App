22a82782010212aa5c8f4011e5aafc05192769df
RoopaChiluvuri <roopasree2509@gmail.com>
2024-08-22 18:19:03 -0700

    adding the update and delete links to the UI

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, HttpResponse
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.urls import reverse_lazy
 
 
 from . import models
@@ -45,6 +46,15 @@ class RecipeUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+
+
+class RecipeDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = models.Recipe
+    success_url = reverse_lazy('recipes-home')
+
+    def test_func(self):
+        recipe = self.get_object()
+        return self.request.user == recipe.author
 
 
 def about(request):
